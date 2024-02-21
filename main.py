2daf23e310c76ed380d2c7d02469c4ccdcd03fbe
codesabersdev <159543459+codesabersdev@users.noreply.github.com>
2024-02-21 09:41:28 +0400

    Code formatting

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ class FPDF(FPDF):
         self.set_font('Arial', 'I', 10)
         # Display page number
         self.cell(0, 10, f'Page {self.page_no()} / {{nb}}', 0, 0, 'C')
-        # page_num = self.page_no()
-        # self.cell(0, 10, str(page_num), align="C")
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
@@ -26,24 +24,29 @@ df = pd.read_csv('topics.csv')
 
 for index, rows in df.iterrows():
     count = 1
+
     for page in range(rows["Pages"]):
         pdf.add_page()
         # Styling for the Main Headings
         pdf.set_font("Times", style="B", size=24)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(0, 12, txt=rows["Topic"], align="L")
+
         # Styling for the header page number
         pdf.set_font("Times", style="I", size=10)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(0, 12, txt=f"Page {count} of {rows['Pages']}", align="R")
         count += 1
-        # For the separator
+
+        # For the header separator
         pdf.set_line_width(1)
         pdf.line(10, 22, 200, 22)
-        # For adding page rulings --- START---
+
+        # --- START--- For adding page rulings
         pdf.set_line_width(0.3)
         for y in range(32, 290, 10):
             pdf.line(10, y, 200, y)
-        # For adding page rulings --- END ---
+        # --- END --- For adding page rulings
 
+# Generating the pdf document
 pdf.output("Python Workbook.pdf")
